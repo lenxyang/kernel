f210a8e930318167a66bf710ecbae8c537fbfcce
yanglei <yanglei@gamil.com>
2013-04-25 12:07:52 +0800

    fixbug

--- rlk/arch/x86/kernel/setup.c
+++ rlk/arch/x86/kernel/setup.c
@@ -1,3 +1,16 @@
 #include <asm/bootparam.h>
+#include <asm/sections.h>
+
+/*
+ * end_pfn only includes RAM, while max_pfn_mapped includes all e820 entries.
+ * The direct mapping extends to max_pfn_mapped, so that we can directly access
+ * apertures, ACPI and other tables without having to play with fixmaps.
+ */
+unsigned long max_low_pfn_mapped;
+unsigned long max_pfn_mapped;
+
+static unsigned long _brk_start = (unsigned long)__brk_base;
+unsigned long _brk_end = (unsigned long)__brk_base;
 
 struct boot_params boot_params;
+
