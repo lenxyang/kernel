7f2612ebfe78c49bc532c25d8eb55c7276fbfeed
yanglei <yanglei@gamil.com>
2013-04-26 10:26:13 +0800

    compile successfully

--- rlk/arch/x86/mm/pgtable_32.c
+++ rlk/arch/x86/mm/pgtable_32.c
@@ -1,5 +1,11 @@
+#include <linux/mm.h>
+#include <asm/pgtable.h>
 
 unsigned int __VMALLOC_RESERVE = 128 << 20;
 
-
 unsigned long __FIXADDR_TOP = 0xfffff000;
+
+void set_pte_vaddr(unsigned long vaddr, pte_t pteval) {
+}
+
+
